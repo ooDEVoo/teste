fcb83594f7598dedcb18aceabd769919cf805264
ooDEVoo <diegopazetto@yahoo.com.br>
2023-03-16 13:09:21 -0300

    Operador ternário

--- teste/src/teste/testee.java
+++ teste/src/teste/testee.java
@@ -4,5 +4,25 @@ public class testee {
 public static void main(String[] args) {
 	System.out.println("hgfgyud");
 	System.out.println("bhisdbfhid");
-}
-}
+	
+	int a,b;
+	a = 5;
+	b = 6;
+	
+	String resultado ="";
+	
+	//Usando IF and ELSE
+	if(a == b) 
+		resultado = "verdadeiro";
+	
+		else 
+			resultado = "falso";
+		
+	//Usando operador ternário
+	 resultado = a == b ? "verdaddeiro" : "falso";
+
+
+	}
+	}
+
+
